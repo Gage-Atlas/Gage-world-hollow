2f7433e114901c23164cffe92ad65b1e96635cc9
Gage-Atlas <Gagebreedon@gmail.com>
2025-02-21 21:20:18 -0500

    Movement update

--- World design hollow/Assets/code/Movement.cs
+++ World design hollow/Assets/code/Movement.cs
@@ -1,43 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    
-    //public float speed = 5.0f;
-    public float jumpForce = 10.0f;
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    private Rigidbody2D rBody;
-    public 
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
-    void Start()
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
+
+    private void Update()
     {
-        rBody = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        Vector2 direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.D))
+        if (isDashing)
         {
-            direction = Vector2.right;
+            return;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            direction = Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            direction = Vector2.up + Vector2.up + Vector2.up;
-        }
-        float dt = Time.deltaTime;
-        float speed = 6.0f;
-        Vector3 change = direction * speed * dt;
-        transform.position = transform.position + change;
-       //rBody.AddForce(jumpForce);
-    }
-  
-    
 
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
 }
